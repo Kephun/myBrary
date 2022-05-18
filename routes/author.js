693e37d1ea416e52d10ec2ab683e54e86633d988
Kevin <kevin.horng422@gmail.com>
2022-05-18 18:23:42 -0400

    Added routing to create and search author pages

---
+++ routes/author.js
@@ -0,0 +1,45 @@
+const { modalUnstyledClasses } = require('@mui/material');
+const express = require('express');
+const router = express.Router();
+const Author = require('../models/author')
+
+//All Author Route
+router.get('/',  async (req, res) => {
+    let searchOptions = {};
+    if (req.query.name != null && req.query.name !=='') {
+        searchOptions.name = new RegExp(req.query.name, 'i')
+    }
+    try {
+        const authors = await Author.find(searchOptions)
+        res.render('author/index', {
+            authors: authors,
+            searchOption: req.query,
+        })
+    } catch {
+        res.redirect('/')
+    }
+});
+
+//New Author
+router.get('/new', (req, res) => {
+    res.render('author/new', { author: new Author() })
+});
+
+//Create Author
+router.post('/', async (req, res) => {
+    const author = new Author({
+        name: req.body.name
+    })
+    try {
+        const newAuthor = await author.save();
+        // res.redirect('authors/${newAuthor.id}')
+        res.redirect('authors');
+    } catch {
+        res.render('author/new', {
+            author: author,
+            errorMessage: 'Error creating author'
+        })
+    }
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -6,24 +6,27 @@ const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
 const indexRouter = require('./routes/index');
-
-
-//Makes sure the DB is connected and throws error is something goes wrong
+const authorRouter = require('./routes/author');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
-const db = mongoose.connection;
-db.on('error', error => console.error(error))
-db.once('open', () => console.log('Connected to Mongoose'))
-
 //import view template / layout, and use them with directions to the files
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
+
+//Makes sure the DB is connected and throws error is something goes wrong
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose'))
 
 //Routes
 app.use('/', indexRouter);
+app.use('/authors', authorRouter);
 
 //Listener
 app.listen(process.env.PORT || 3000);
