7636413d0aef48a1e04f16ff2cc0d274a6081426
Kevin <kevin.horng422@gmail.com>
2022-05-18 13:40:56 -0400

    Connection to heroku

--- server.js
+++ server.js
@@ -6,13 +6,16 @@ const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
 const indexRouter = require('./routes/index');
-const mongoose = require('mongoose');
 
+
+//Makes sure the DB is connected and throws error is something goes wrong
+const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
+//import view template / layout, and use them with directions to the files
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
@@ -22,7 +25,7 @@ app.use(express.static('public'));
 //Routes
 app.use('/', indexRouter);
 
-
+//Listener
 app.listen(process.env.PORT || 3000);
 
 
