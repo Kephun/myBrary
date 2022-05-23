2b280f174e90b77f99a59e6c527c928b4d74c804
Kevin <kevin.horng422@gmail.com>
2022-05-23 15:15:47 -0400

    Added book model, route, and views

--- server.js
+++ server.js
@@ -6,8 +6,10 @@ const app = express();
 const expressLayouts = require('express-ejs-layouts');
 const indexRouter = require('./routes/index');
 const authorRouter = require('./routes/author');
+const bookRouter = require('./routes/books')
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+
 //import view template / layout, and use them with directions to the files
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
@@ -26,6 +28,7 @@ db.once('open', () => console.log('Connected to Mongoose'))
 //Routes
 app.use('/', indexRouter);
 app.use('/authors', authorRouter);
+app.use('/books', bookRouter)
 
 //Listener
 app.listen(process.env.PORT || 3000);
