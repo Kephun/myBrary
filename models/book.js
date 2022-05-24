9c8966ceeab785fb5cc2d5243e34fc76926829d1
Kevin <kevin.horng422@gmail.com>
2022-05-24 11:59:21 -0400

    Uploading files with pondfile w/ ecoded 64

--- models/book.js
+++ models/book.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
-const path = require('path');
 
-const coverImageBasePath = 'uploads/bookCovers';
 
 const bookSchema = new mongoose.Schema({
     title: {
@@ -24,22 +22,26 @@ const bookSchema = new mongoose.Schema({
         required: true,
         default: Date.now //Creates a date on the day it was created
     },
-    coverImageName: {
+    coverImage: {
+        type: Buffer,
+        required: true,
+    },
+    coverImageType: {
         type: String,
         required: true,
     },
     author: {
         type: mongoose.Schema.Types.ObjectId, //Reference another object
         required: true,
+        ref: 'Author'
     }
 });
 
 bookSchema.virtual('coverImagePath').get(function() {
-    if(this.coverImageName != null) {
-        return path.join('/', coverImageBasePath, this.coverImageName)
+    if(this.coverImage != null && this.coverImageType != null) {
+        return `data:${this.coverImageType};charset=utf-8;base64,${this.coverImage.toString('base64')}`
     }
 })
 
 module.exports = mongoose.model('Book', bookSchema);
 
-module.exports.coverImageBasePath = coverImageBasePath;
