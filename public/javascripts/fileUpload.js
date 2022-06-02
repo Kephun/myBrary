36b062dfa7244a0c181e9f096dbe8ba826c4b763
Kevin <kevin.horng422@gmail.com>
2022-06-02 12:18:28 -0400

    Added styling for buttons and index page

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -8,6 +8,7 @@ FilePond.setOptions({
     stylePanelAspectRatio:150 / 100,
     imageResizeTargetWidth: 100,
     imageResizeTargetHeight: 150,
+    imagePreviewMaxHeight: 10,
 })
 
 FilePond.parse(document.body);
